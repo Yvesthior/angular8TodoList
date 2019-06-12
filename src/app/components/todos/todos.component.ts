b128a6e6c0c6c1c7abb08a7ebaa18ccb331e695f
Yves Robert Charles THIOR <yvesthior@gmail.com>
2019-06-12 21:35:04 +0000

    Added Todos and Fetching

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../model/Todos';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+todos: { id: number; title: string; completed: boolean }[] = [];
   constructor() { }
 
   ngOnInit() {
+    return this.todos = [
+      {
+        id: 1,
+        title: 'todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'todo two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'todo three',
+        completed: false
+      }
+    ];
   }
 
 }
