8b6589296d601334bb3085359305ac87dc4b3f2d
Yves Robert Charles THIOR <yvesthior@gmail.com>
2019-06-12 23:27:03 +0000

    Added todo Service to provide Todo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Input} from '@angular/core';
+import {Todo} from '../../model/Todos';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+@Input() todo: Todo;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setClasses() {
+    const classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    };
+    return classes;
+  }
+
+  onToggle() {
+    this.todo.completed = !this.todo.completed;
+  }
+
+  onDelete() {
+    console.log('Deleted');
   }
 
 }
